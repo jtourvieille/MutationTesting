74259c0a169aec7015bedd68d3b22f21de62d21e
jtourvieille <jtourvieille@gmail.com>
2019-05-15 15:40:47 +0200

    add dotnet framework solution

---
+++ src/net45/FizzBuzz/FizzBuzz.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SESD")]
+[assembly: AssemblyProduct("FizzBuzz.Tests")]
+[assembly: AssemblyCopyright("Copyright © SESD 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("103b6917-d30a-49bc-a19b-493868347499")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/netcore/FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,89 @@
+﻿using FizzBuzz.Impl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void Should_Say1_When_1IsGivenAsInput()
+        {
+            // Arrange
+            var fizzBuzz = new FizzBuzzService();
+
+            // Act
+            var output = fizzBuzz.Play(1);
+
+            // Assert
+            Assert.AreEqual("1", output);
+        }
+
+        // Those tests are intentionnaly commented
+
+        //[TestMethod]
+        //public void Should_Say12_When_2IsGivenAsInput()
+        //{
+        //    // Arrange
+        //    var fizzBuzz = new FizzBuzzService();
+
+        //    // Act
+        //    var output = fizzBuzz.Play(2);
+
+        //    // Assert
+        //    Assert.AreEqual("12", output);
+        //}
+
+        //[TestMethod]
+        //public void Should_Say12Fizz_When_3IsGivenAsInput()
+        //{
+        //    // Arrange
+        //    var fizzBuzz = new FizzBuzzService();
+
+        //    // Act
+        //    var output = fizzBuzz.Play(3);
+
+        //    // Assert
+        //    Assert.AreEqual("12Fizz", output);
+        //}
+
+        //[TestMethod]
+        //public void Should_Say12Fizz4_When_4IsGivenAsInput()
+        //{
+        //    // Arrange
+        //    var fizzBuzz = new FizzBuzzService();
+
+        //    // Act
+        //    var output = fizzBuzz.Play(4);
+
+        //    // Assert
+        //    Assert.AreEqual("12Fizz4", output);
+        //}
+
+        //[TestMethod]
+        //public void Should_Say12Fizz4Buzz_When_5IsGivenAsInput()
+        //{
+        //    // Arrange
+        //    var fizzBuzz = new FizzBuzzService();
+
+        //    // Act
+        //    var output = fizzBuzz.Play(5);
+
+        //    // Assert
+        //    Assert.AreEqual("12Fizz4Buzz", output);
+        //}
+
+        //[TestMethod]
+        //public void Should_Say12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz_When_15IsGivenAsInput()
+        //{
+        //    // Arrange
+        //    var fizzBuzz = new FizzBuzzService();
+
+        //    // Act
+        //    var output = fizzBuzz.Play(15);
+
+        //    // Assert
+        //    Assert.AreEqual("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz", output);
+        //}
+    }
+}
